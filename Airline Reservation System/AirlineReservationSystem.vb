1dcb2bba5a44ca15f97552aca90cfd1d1707d753
emir-karpuz <emirkarpuz.megayazilim@outlook.com>
2021-06-01 17:11:46 +0300

    Flight class added

--- Airline Reservation System/AirlineReservationSystem.vb
+++ Airline Reservation System/AirlineReservationSystem.vb
@@ -1,11 +1,22 @@
-﻿Module AirlineReservationSystem
+﻿Imports Airline_Reservation_System
+
+Module AirlineReservationSystem
+
+    Sub Main()
+        Dim PegasusAircraft As New Aircraft(1, "Airbus", "A320")
+        Dim AnadoluJetAircraft As New Aircraft(1, "Boeing", "737")
+
+        Dim TK2186 As New Flight(1, "TK2186", #2021-6-1#, #13:15:00#, "TZX", "SAW", PegasusAircraft)
+        TK2186.Schedule()
+
+        Console.ReadLine()
+    End Sub
 
     Public Class Aircraft
-
-        Private ReadOnly Property Id() As Integer
-        Private ReadOnly Property NameOfManifacturer() As String
-        Private ReadOnly Property Model() As String
-        Private ReadOnly Property NumberOfSeats() As Integer
+        Private ReadOnly Property Id As Integer
+        Private ReadOnly Property NameOfManifacturer As String
+        Private ReadOnly Property Model As String
+        Private ReadOnly Property NumberOfSeats As Integer
 
         Public Sub New(id As Integer, nameOfManifacturer As String, model As String)
             Me.Id = id
@@ -25,17 +36,48 @@
         Public Sub Land()
             Console.WriteLine(Me.NameOfManifacturer & " " & Me.Model & " is landing.")
         End Sub
-
     End Class
 
-    Sub Main()
-        Dim PegasusAircraft As New Aircraft(1, "Airbus", "A320")
-        PegasusAircraft.Takeoff()
+    Public Class Flight
+        Private ReadOnly Property Id As Integer
+        Private ReadOnly Property Number As String
+        Private ReadOnly Property DateOfDeparture As Date
+        Private ReadOnly Property TimeOfDeparture As Date
+        Private ReadOnly Property WhereFrom As String
+        Private ReadOnly Property WhereTo As String
+        Private ReadOnly Property TypeOfAircraft As Aircraft
+        Private ReadOnly Property Availability As Boolean
 
-        Dim AnadoluJetAircraft As New Aircraft(1, "Boeing", "737")
-        AnadoluJetAircraft.Takeoff()
+        Public Sub New(id As Integer,
+                       number As String,
+                       dateOfDeparture As Date,
+                       timeOfDeparture As Date,
+                       whereFrom As String,
+                       whereTo As String,
+                       typeOfAircraft As Aircraft)
+            Me.Id = id
+            Me.Number = number
+            Me.DateOfDeparture = dateOfDeparture
+            Me.TimeOfDeparture = timeOfDeparture
+            Me.WhereFrom = whereFrom
+            Me.WhereTo = whereTo
+            Me.TypeOfAircraft = typeOfAircraft
+        End Sub
 
-        Console.ReadLine()
-    End Sub
+        Public Sub Schedule()
+            Console.WriteLine("The flight number " & Me.Number & " has been scheduled." & vbCrLf &
+                              "Id number is " & Me.Id & vbCrLf &
+                              "Departure date is " & Me.DateOfDeparture & vbCrLf &
+                              "Departure time is " & Me.TimeOfDeparture)
+        End Sub
+
+        Public Sub Delay()
+            Console.WriteLine("The flight number " & Me.Number & " has been delayed.")
+        End Sub
+
+        Public Sub Cancel()
+            Console.WriteLine("The flight number " & Me.Number & " has been canceled.")
+        End Sub
+    End Class
 
 End Module
