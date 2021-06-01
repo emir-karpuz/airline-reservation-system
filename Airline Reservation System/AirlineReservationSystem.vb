a9061f657246ebf85e9aeda9d738f986923e69d4
emir-karpuz <emirkarpuz.megayazilim@outlook.com>
2021-06-01 17:26:38 +0300

    Seat class added

--- Airline Reservation System/AirlineReservationSystem.vb
+++ Airline Reservation System/AirlineReservationSystem.vb
@@ -1,13 +1,12 @@
-﻿Imports Airline_Reservation_System
-
-Module AirlineReservationSystem
+﻿Module AirlineReservationSystem
 
     Sub Main()
         Dim PegasusAircraft As New Aircraft(1, "Airbus", "A320")
         Dim AnadoluJetAircraft As New Aircraft(1, "Boeing", "737")
-
         Dim TK2186 As New Flight(1, "TK2186", #2021-6-1#, #13:15:00#, "TZX", "SAW", PegasusAircraft)
-        TK2186.Schedule()
+        Dim WindowSeat As New Seat(1, "F3", TK2186, "Economic")
+
+        WindowSeat.Book()
 
         Console.ReadLine()
     End Sub
@@ -22,11 +21,13 @@ Module AirlineReservationSystem
             Me.Id = id
             Me.NameOfManifacturer = nameOfManifacturer
             Me.Model = model
+
             If Me.Model = "A320" Then
                 Me.NumberOfSeats = 180
             ElseIf Me.Model = "737" Then
                 Me.NumberOfSeats = 170
             End If
+
         End Sub
 
         Public Sub Takeoff()
@@ -77,6 +78,35 @@ Module AirlineReservationSystem
 
         Public Sub Cancel()
             Console.WriteLine("The flight number " & Me.Number & " has been canceled.")
+        End Sub
+    End Class
+
+    Public Class Seat
+        Private ReadOnly Property Id As Integer
+        Private ReadOnly Property Number As String
+        Private ReadOnly Property NumberOfFlight As Flight
+        Private ReadOnly Property Type As String
+        Private ReadOnly Property Price As Double
+        Private ReadOnly Property Availability As Boolean
+
+        Public Sub New(id As Integer, number As String, numberOfFlight As Flight, type As String)
+            Me.Id = id
+            Me.Number = number
+            Me.NumberOfFlight = numberOfFlight
+            Me.Type = type
+
+            If Me.Type = "Economic" Then
+                Me.Price = 249.99
+            ElseIf Me.Type = "Business" Then
+                Me.Price = "599.99"
+            End If
+
+        End Sub
+
+        Public Sub Book()
+            Console.WriteLine("The seat number " & Me.Number & " has been booked." & vbCrLf &
+                                "Seat type is " & Me.Type & vbCrLf &
+                                "Price is " & Me.Price)
         End Sub
     End Class
 
